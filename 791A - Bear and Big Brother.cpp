cd31e48a3d5ed6d02eb1fca2c225d6f0cd8670c4
Atik Jawad <85114480+AtikJawad@users.noreply.github.com>
2024-07-14 11:26:56 +0600

    Create 791A - Bear and Big Brother.cpp

---
+++ 791A - Bear and Big Brother.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a,b; // a=Limak's weight & b=Bob's weight
+cin>>a>>b;
+
+int years=0; // counter for years
+
+for(int i=0; a<=b; i++){ // loop goes on as long as a is not greater than b
+
+        a=3*a; // Limak weighs triple each year & Bob twice
+        b=2*b;
+        years++;
+}
+cout<<years; // output
+
+return 0;
+
+}
