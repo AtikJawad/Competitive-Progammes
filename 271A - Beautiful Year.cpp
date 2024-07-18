d686c4989105067706700cfa8491c4ae51a0c3b9
Atik Jawad <85114480+AtikJawad@users.noreply.github.com>
2024-07-18 13:07:08 +0600

    Update 271A - Beautiful Year.cpp

--- 271A - Beautiful Year.cpp
+++ 271A - Beautiful Year.cpp
@@ -5,20 +5,20 @@ int main(){
  int y;
  cin>>y;
 
- while(y++){ 
+ while(y++){ // here only increment operator is used in the while loop
 
-     int a=y/1000;
-     int b=(y/100)%10;
-     int c= (y/10)%10;
-     int d=y%10;
+     int a=y/1000;   // assigning 1st digit of y to a
+     int b=(y/100)%10;  //assigning 2nd digit of y to b 
+     int c= (y/10)%10;   //assigning 3rd digit of y to c 
+     int d=y%10;   //assigning last digit of y to d
 
      if(a!=b && a!=c && a!=d && b!=c && b!=d && c!=d){
 
-        break;
+        break;     // if all the digits do not match to each other then break the loop
      }
  }
 
- cout<<y;
+ cout<<y; // Output
 
  return 0;
 
