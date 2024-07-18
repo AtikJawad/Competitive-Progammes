44cdda2511d058eeb12038a0f55edf0e3af4e605
Atik Jawad <85114480+AtikJawad@users.noreply.github.com>
2024-07-18 13:21:32 +0600

    Update 339A - Helpful Maths.cpp

--- 339A - Helpful Maths.cpp
+++ 339A - Helpful Maths.cpp
@@ -4,28 +4,29 @@
 using namespace std;
 
 int main(){
- char str[101];
- cin.getline(str,101);
+ string str; // used string here instead of character array as it was getting error (don't know why)
+ cin>>str;
+ int n= str.length();
  char temp;
 
 
-for(int i=0;str[i]!='\0';i+=2){
+for(int i=0;i<n;i+=2){ // comparing even i'th index with even j'th index bcz odd index contains '+'
     if(str[1]=='\0'){
-        break;
+        break; //checks if the 2nd index is null (checks if the input has only 1 digit or not)
     }
 
-    for(int j=i+2;str[j]!='\0';j+=2){
+    for(int j=i+2;j<n;j+=2){
 
-        if(str[i]>str[j]){
+        if(str[i]>str[j]){ // used the bubble sort to swap the digits numerically
 
             temp=str[i];
             str[i]=str[j];
             str[j]=temp;
         }
-        
+
     }
 }
-cout<<str;
+cout<<str; // output
 return 0;
 
 
