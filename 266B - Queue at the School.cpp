1432b09dee55dd35b3df61e6364339ac0e2dc344
Atik Jawad <85114480+AtikJawad@users.noreply.github.com>
2024-08-09 11:33:14 +0600

    Update 266B - Queue at the School.cpp

--- 266B - Queue at the School.cpp
+++ 266B - Queue at the School.cpp
@@ -10,14 +10,17 @@ int main(){
  for(int sec=0;sec<t;sec++){
     for(int i=0;i<n;i++){
         if(line[i] < line[i+1]){
-            swap(line[i] , line[i+1]);
-            i++;
+            swap(line[i] , line[i+1]); // swap() is a built-in function in cpp
+         
+            i++;   /*When you find a BG pair in the queue and swap them, you need to ensure that the newly swapped pair (now GB) is not considered for swapping again in the same iteration of the inner loop. 
+                   If you don't increment i, the loop would check the new positions as well, potentially swapping them again within the same second, which is not desired. That's why it's necessary to increment in in the if statement.
+                  */
         }
     }
  }
 
  for(int i=0;i<n;i++){
-    cout<<line[i];
+    cout<<line[i]; // Output
  }
 
 
